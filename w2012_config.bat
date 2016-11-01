d33275f48b48be0618be8374dec522f20b439438
kentarok <kentarok009@gmail.com>
2016-11-01 16:49:35 +0900

    Update w2012_config.bat

--- w2012_config.bat
+++ w2012_config.bat
@@ -2,6 +2,6 @@
 
 rem netsh advfirewall set allprofiles state on
 powershell -Command "winrm quickconfig -q"
-echo "10.122.221.160 sjchefs01.cent7.local" >> %windir%\system32\drivers\etc\hosts
+echo 10.122.221.160 sjchefs01.cent7.local >> %windir%\system32\drivers\etc\hosts
 
 exit
