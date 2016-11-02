fb3d4ead2c48de444a2e400fb89ed717de865b29
kentarok <kentarok009@gmail.com>
2016-11-02 11:11:26 +0900

    Update w2012_config.bat

--- w2012_config.bat
+++ w2012_config.bat
@@ -1,7 +1,10 @@
 @echo off
 
+set IP=10.122.221.160
+set FQDN=sjchefs01.cent7.local
+
 rem netsh advfirewall set allprofiles state on
 powershell -Command "winrm quickconfig -q"
-echo 10.122.221.160 sjchefs01.cent7.local >> %windir%\system32\drivers\etc\hosts
+echo %IP% %FQDN% >> %windir%\system32\drivers\etc\hosts
 
 exit
