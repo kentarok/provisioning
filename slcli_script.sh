0b52e51eb515ab5db2b85842b19ef122ef8843a6
kentarok <kentarok009@gmail.com>
2017-02-09 15:22:16 +0900

    Update slcli_script.sh

--- slcli_script.sh
+++ slcli_script.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
 ## Chef WS Virtual Server
-#slcli vs create --test \
-# --hostname=tvcws01 --domain=tvc.local \
-# --datacenter=tok02 --billing=monthly --os=CENTOS_LATEST_64 \
+#slcli vs create \
+# --domain=tvc.local --datacenter=tok02 --billing=monthly --os=CENTOS_LATEST_64 \
 # --cpu=2 --memory=4096 --disk=100 --network=100 \
 # --postinstall=https://raw.githubusercontent.com/kentarok/provisioning/master/tvcchefw_config \
-# --key=chefws_public --vlan-private=xxxxxxx
+# --key=chewsf_public --vlan-private=xxxxxxx \
+# --test --hostname=tvcws01
 
-## Chef Node Virtual Server
-for i in {1..26};\
-  do name=$(printf tvcnode%03d $i);\
-    slcli vs create --test \
-     --hostname=$tvcnode --domain=tvc.local \
-     --datacenter=tok02 --billing=hourly --os=CENTOS_LATEST_64 \
-     --cpu=1 --memory=1024 --disk=100 --network=100 \
-     --postinstall=https://raw.githubusercontent.com/kentarok/provisioning/master/tvcchefn_config \
-     --key=chefnode_public --vlan-private=xxxxxxx
-done;
-
-#slcli vs create --test \
-# --hostname=tvcnode --domain=tvc.local \
-# --datacenter=tok02 --billing=hourly --os=CENTOS_LATEST_64 \
-# --cpu=1 --memory=1024 --disk=100 --network=100 \
-# --postinstall=https://raw.githubusercontent.com/kentarok/provisioning/master/tvcchefn_config \
-# --key=chefnode_public --vlan-private=xxxxxxx
+## Chef Node 1~26 Virtual Server
+for i in {1..26}
+  do name=$(printf tvcnode%02d $i)
+    echo "\
+slcli vs create \
+ --domain=tvc.local --datacenter=tok02 --billing=hourly --os=CENTOS_LATEST_64 \
+ --cpu=1 --memory=1024 --disk=25 --network=100 \
+ --postinstall=https://raw.githubusercontent.com/kentarok/provisioning/master/tvcchefn_config \
+ --key=chefn_public --vlan-private=xxxxxxx \
+ --test --hostname=$name \n"
+done
