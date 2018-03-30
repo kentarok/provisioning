97e5ca53536e13ab53987a3f615f522ae021019b
kentarok <kentarok009@gmail.com>
2018-03-30 15:55:46 +0900

    Update chefws.rb

--- handson/chefws.rb
+++ handson/chefws.rb
@@ -25,8 +25,34 @@ userdata.each do | attr |
   user attr[0] do
     uid attr[1].to_i
     gid attr[2]
+    manage_home true
+  end
+  
+  directory "/home/#{attr[0]}/.ssh" do
+    owner attr[0]
+    group attr[2]
   end
 end
+
+remote_file '/home/kentarok/.ssh/id_handson' do
+  source 'https://raw.githubusercontent.com/kentarok/provisioning/master/handson/id_handson'
+end
+
+file '/home/kentarok/.ssh/authorized_keys' do
+  content IO.read('/root/.ssh/authorized_keys')
+  owner 'kentarok'
+  owner '1999'
+end
+
+%w(0 1 2).each do | i |
+  file "/home/#{userdata[i.to_i][0]}/.ssh/authorized_keys" do
+    content IO.read('/home/kentarok/.ssh/id_handson')
+    owner "#{userdata[i.to_i][0]}"
+    group "#{userdata[i.to_i][2]}"
+  end
+end
+
+__END__
 
 service "sshd"
 
@@ -43,6 +69,3 @@ file "/etc/sudoers" do
   f.insert_line_if_no_match(/^%myadms ALL=(ALL) NOPASSWD: ALL/, '%myadms ALL=(ALL) NOPASSWD: ALL')
   content f.send(:editor).lines.join
 end
-
-
-
