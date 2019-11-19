0c46ac9b1e71eb63681dc4e43d157361455ec2fa
kentarok <kentarok009@gmail.com>
2019-11-19 18:12:18 +0900

    Update controlnode.sh

--- ansible/controlnode.sh
+++ ansible/controlnode.sh
@@ -56,3 +56,12 @@ echo "%${GNAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${GNAME}
 ## Disable text passwords
 sed -i.org -e "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
 service sshd restart
+
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+pip3 install ansible ansible-lint
+
+#cd ~/ && curl -O https://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-latest.tar.gz
+#tar xvzf ansible-tower-setup-latest.tar.gz
+#curl https://raw.githubusercontent.com/kentarok/provisioning/master/ansible/inventory > ./ansible-tower-setup-*/inventory
+#~/ansible-tower-setup-*/setup.sh
