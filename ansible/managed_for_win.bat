44c63a16af450701f95e386e505a19919ad5f3a6
kentarok <kentarok009@gmail.com>
2019-11-20 19:12:45 +0900

    Update managed_for_win.bat

--- ansible/managed_for_win.bat
+++ ansible/managed_for_win.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\PostScript\wget.exe https://github.com/ansible/ansible/blob/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 --no-check-certificate
+C:\PostScript\wget.exe https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 --no-check-certificate
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Unrestricted C:\PostScript\ConfigureRemotingForAnsible.ps1
